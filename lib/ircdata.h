cbdd5113df636f2f68161f0e279c0b8dd17dda54
chaoticweg <shawn@chaoticweg.cc>
2017-05-02 19:44:13 -0500

    format, add errors, add create function

--- lib/ircdata.h
+++ lib/ircdata.h
@@ -7,11 +7,19 @@
 #pragma once
 
 
+// errors
+#define IRCDATA_CONNREFUSED  -1
+#define IRCDATA_UNSUPPORTED  -2
+
+
+// data
 #define IRCDATA_LEAVE  0
 #define IRCDATA_JOIN   1
 #define IRCDATA_MSG    2
 #define IRCDATA_FILE   3
 
+
+// constraints
 #define IRCDATA_MAXLEN  256
 
 
@@ -22,3 +30,16 @@ struct ircdata_t
     char *contents;
     int contents_length;
 };
+
+
+/** Helper method: create struct ircdata_t with the given info */
+struct ircdata_t ircdata_create(int type, char *contents, int contents_length)
+{
+    struct ircdata_t result;
+
+    result.type            = type;
+    result.contents        = contents;
+    result.contents_length = contents_length;
+
+    return result;
+}
