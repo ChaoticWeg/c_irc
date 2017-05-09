6a8f3320139779473d0f73d5415c330e997197a0
chaoticweg <shawn@chaoticweg.cc>
2017-05-09 02:06:52 -0500

    HELPER METHODS
    lots of em

--- lib/ircdata.h
+++ lib/ircdata.h
@@ -16,16 +16,18 @@
 
 
 // data
-#define IRCDATA_LEAVE      0
-#define IRCDATA_JOIN       1
-#define IRCDATA_MSG        2
-#define IRCDATA_FILE       3
-#define IRCDATA_FILE_DONE  4
+#define IRCDATA_LEAVE      1
+#define IRCDATA_JOIN       2
+#define IRCDATA_MSG        3
+#define IRCDATA_FILE       4
+#define IRCDATA_FILE_DONE  5
+#define IRCDATA_FILE_OK    6
 
 
 // constraints
 #define IRCDATA_MAXLEN      2000
 #define USERNAME_MAXLEN       15
+#define FILENAME_MAXLEN       15
 #define IRCDATA_FILE_BUFLEN  256
 
 
@@ -35,9 +37,10 @@ struct ircdata_t
     int type;
 
     char username[USERNAME_MAXLEN + 1];
+    char filename[FILENAME_MAXLEN + 1];
     
     int contents_length;
-    char contents[IRCDATA_MAXLEN];
+    char contents[IRCDATA_MAXLEN + 1];
 };
 
 
@@ -54,4 +57,26 @@ struct ircdata_t ircdata_create(int type, char *contents)
     result.contents_length = strlen(result.contents) + 1;
 
     return result;
+}
+
+void ircdata_copy_file_contents(struct ircdata_t *result, char *contents)
+{
+    bzero(result->contents, IRCDATA_MAXLEN + 1);
+    strncpy(result->contents, contents, IRCDATA_MAXLEN);
+    result->contents[IRCDATA_MAXLEN] = '\0';
+    result->contents_length = strlen(result->contents) + 1;
+}
+
+void ircdata_copy_username(struct ircdata_t *result, char *username)
+{
+    bzero(result->username, USERNAME_MAXLEN + 1);
+    strncpy(result->username, username, USERNAME_MAXLEN);
+    result->username[USERNAME_MAXLEN] = '\0';
+}
+
+void ircdata_copy_filename(struct ircdata_t *result, char *filename)
+{
+    bzero(result->filename, FILENAME_MAXLEN + 1);
+    strncpy(result->filename, filename, FILENAME_MAXLEN);
+    result->filename[FILENAME_MAXLEN] = '\0';
 }
